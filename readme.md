aba2b8c569e022cb923110111d1e3247674fcf8e
xhrbtk <xhtbtk@gmail.com>
2023-12-22 23:00:10 +0800

    feat:增加gitignore文件

---
+++ readme.md
@@ -0,0 +1,5 @@
+## yarn install
+
+## 启动服务端 node server.js
+
+## 启动客户端 http-server -p9090 需要本地安装http-servers 
---
+++ server.js
@@ -0,0 +1,33 @@
+
+const WebSocket = require('ws');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log(`Received: ${message}`);
+        // 广播消息给所有连接的客户端
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// 静态文件服务（可选）
+app.use(express.static('public'));
+
+server.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000');
+});
